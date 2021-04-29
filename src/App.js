e0960d24ba480517c16737e5055d0ae8c94ae2b8
Rheinard Korf <rheinard.korf@xwp.co>
2021-04-29 14:22:20 +1000

    Tweak.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cleanup test!!!!
+          Cool things.
         </a>
       </header>
     </div>
