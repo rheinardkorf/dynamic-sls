11c7cd40d8d5ba8f44c7d894513785033f666033
Rheinard Korf <rheinard.korf@xwp.co>
2021-04-29 14:16:15 +1000

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          DYNAMIC!!!!
+          Cleanup test!!!!
         </a>
       </header>
     </div>
