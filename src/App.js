e37863d01bd44677a1ffb9d4ccefcd31e9097e99
Rheinard Korf <rheinard.korf@xwp.co>
2021-04-29 11:51:23 +1000

    test delete.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          DYNAMIC!!!!
+          Test delete.
         </a>
       </header>
     </div>
